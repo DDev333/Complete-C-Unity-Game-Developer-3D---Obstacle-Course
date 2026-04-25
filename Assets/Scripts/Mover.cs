df5647ac110427a6b31a746db7325afc4038d42b
Devesh Ghodpage <96539627+DDev333@users.noreply.github.com>
2026-04-25 18:10:07 +0530

    Initial check-in

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PrintInst();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movePlayer();
+    }
+
+    void PrintInst()
+    {
+        Debug.Log("Welcome to the Obstacle Course");
+        Debug.Log("Move the cube with arrow keys or WASD");
+        Debug.Log("Don't hit the walls");
+    }
+
+    void movePlayer()
+    {
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        transform.Translate(xValue, 0, zValue);
+    }
+}
